999c75c2f2d396ab192b6c9d8b7f12ec3089d6ea
Logan Willis <loganw0315@gmail.com>
2021-10-18 09:29:03 -0500

    Add to Zero challenge completed with Celena Rock

---
+++ addToZero.js
@@ -0,0 +1,18 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+numZero = false
+
+for (let i = 0; i < array.length; i++){
+    for(let x = 0; x < array.length; x++){
+        if(array[i] + array[x] === 0){
+            numZero = true
+        }
+        if(array[i] + array[i] === 0){
+            numZero = false
+        }
+    }
+}
+
+console.log(numZero);
