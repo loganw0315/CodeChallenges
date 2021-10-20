465f9f51e5793981367f4c764d9c2bcc4a418db9
Logan Willis <loganw0315@gmail.com>
2021-10-20 09:52:14 -0500

    Add to Zero challenge completed with Celena Rock

--- findTheRange.js
+++ findTheRange.js
@@ -1,5 +1,5 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, -36]
 
-// Write your solution below:
+// Write your solution below
 console.log(Math.min.apply(null,array),",",Math.max.apply(null,array));
