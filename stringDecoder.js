39c66566d95180c213ffffa1dbd8944071cd3b8a
Logan Willis <loganw0315@gmail.com>
2021-10-27 10:40:27 -0500

    String Decoder completed with James Proctor

--- stringDecoder.js
+++ stringDecoder.js
@@ -1,1 +1,22 @@
-const decoder
+const decoder = code => {
+    let Arr = code.split('');
+    newArr = [];
+    let i = 0;
+    while( i < Arr.length ){
+        if(isNaN(Number(Arr[i]))){
+            newArr.push(Arr[i])
+        }
+        
+        i +=  Number(Arr[i]) + 1;
+        newArr.push(Arr[i]);
+        i += 1;
+
+    }
+
+    let decoded = newArr.join('')
+    return decoded;
+
+}
+
+
+console.log(decoder('0y4akjfes'));
